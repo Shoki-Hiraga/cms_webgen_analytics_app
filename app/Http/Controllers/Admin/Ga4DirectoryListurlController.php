54114c5413a4a82ca0b82960153a599153ff8b5f
shoki-hiraga <chaser.cb750@gmail.com>
2025-08-05 17:30:14 +0900

    ファイル名変更

--- app/Http/Controllers/Admin/Ga4DirectoryListurlController.php
+++ app/Http/Controllers/Admin/Ga4DirectoryListurlController.php
@@ -10,12 +10,12 @@ class Ga4DirectoryListurlController extends Controller
     public function index()
     {
         $urls = Ga4DirectoryListurl::orderByDesc('created_at')->paginate(10);
-        return view('admin.ga4_directory_listurls.index', compact('urls'));
+        return view('admin.ga4_directory_listurls.ga4_d_index', compact('urls'));
     }
 
     public function show($id)
     {
         $url = Ga4DirectoryListurl::findOrFail($id);
-        return view('admin.ga4_directory_listurls.show', compact('url'));
+        return view('admin.ga4_directory_listurls.ga4_d_show', compact('url'));
     }
 }
